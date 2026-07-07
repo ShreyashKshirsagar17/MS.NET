cbfd1af359a190c490df2e5d1214657e80250da5
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-07-07 22:45:47 +0530

    Add files via upload

---
+++ 03Demo_Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03Demo_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CMath c = new AdMath();
+            c.Add();
+            c.Sub();
+        }
+    }
+
+    #region Inheritance,Virtual & Override, new KeyWord
+    public class CMath
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine(10 + 20);
+        }
+
+        public void Sub()
+        {
+            Console.WriteLine(50 - 20);
+        }
+    }
+
+    public class AdMath : CMath
+    {
+        public override void Add()
+        {
+            Console.WriteLine(10 * 20);
+        }
+
+        public new void Sub()
+        {
+            Console.WriteLine((50 - 10) * 20);
+        }
+    }
+    #endregion
+}
